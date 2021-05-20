ef38d313dc04f9e36b14f7b5c211910f04ee2679
Charlotte Ulrich <charlotte.ulrich93@gmail.com>
2021-05-20 14:04:14 -0500

    feat(styling) added instructions and styling to waiting and game room

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,16 +1,16 @@
-import React from "react";
-import { connect } from "react-redux";
-import { Link } from "react-router-dom";
-import { createRoom, joinRoom } from "../socket";
-import Footer from "./Footer";
-import history from "../history";
-import Swal from "sweetalert2";
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { createRoom, joinRoom } from '../socket';
+import Footer from './Footer';
+import history from '../history';
+import Swal from 'sweetalert2';
 
 export class Home extends React.Component {
   constructor() {
     super();
     this.state = {
-      roomKey: "",
+      roomKey: '',
     };
     this.handleCreate = this.handleCreate.bind(this);
     this.enterNewRoom = this.enterNewRoom.bind(this);
@@ -43,9 +43,9 @@ export class Home extends React.Component {
 
   displayInstructions() {
     Swal.fire({
-      title: "HOW TO PLAY:",
+      title: 'HOW TO PLAY:',
       html:
-        "Each player will have two 25-second turns to compose a section of an original song. The last two notes from each turn will be passed along to the next player to continue the song. <br></br>" +
+        'Each player will have two 45-second turns to compose a section of an original song. The last two notes from each turn will be passed along to the next player to continue the song. <br></br>' +
         "To compose your section, click on the Sequencer's buttons. Use the menu at the top to change Octaves and Synths.<br></br>" +
         "At the end of the game, you'll be able to hear and download your grammy-nominated masterpiece!",
       showCloseButton: true,
@@ -61,6 +61,12 @@ export class Home extends React.Component {
         <div className="home-page">
           <div className="banner">
             <h2 className="home-title">SynthCity</h2>
+            <h3 className="home-subheading">
+              Make music with your friends before the timer runs out! <br></br>{' '}
+              Your sick tunes will be passed to the next player until everyone
+              has rocked out. <br></br>When the game is done, you'll have an
+              award-winning masterpiece.
+            </h3>
           </div>
           <div className="options-container">
             <div className="column">
@@ -87,7 +93,7 @@ export class Home extends React.Component {
               </div>
             </div>
             <div className="column">
-              <Link to={"/practice"}>
+              <Link to={'/practice'}>
                 <div className="option-card">
                   <h3>Try it out</h3>
                 </div>
@@ -95,11 +101,13 @@ export class Home extends React.Component {
             </div>
           </div>
           <div className="instructions-container">
+            {/* <p>How to Play</p> */}
             <button
               type="button"
               className="instructions-button"
               onClick={this.displayInstructions}
             >
+              {/* How to play */}
               <img src="./InstructionsIcon.png" className="instructions-img" />
             </button>
           </div>
